999932fc8cfa4dfb103153821690b16df504ac05
son.geon <son.geon@jtbc.co.kr>
2025-09-11 08:42:26 +0900

    공통 에러 코드 형식 변경

--- src/main/java/com/stock/batch/batchJob/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stock/batch/batchJob/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.stock.batch.batchJob.exception;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,14 +14,34 @@ import java.util.Map;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
+    private final String errorForm = "%s (%s)";
+
     @ExceptionHandler(CustomApiException.class)
     public ResponseEntity<Map<String, Object>> handleCustomApiException(CustomApiException ex) {
+
         Map<String, Object> body = Map.of(
-                "status", ex.getStatus().value(),
-                "error", ex.getStatus().getReasonPhrase(),
-                "message", ex.getMessage()
+                "errorCode", ex.getStatus().value(),
+                "errorMessage",  String.format(errorForm, ex.getStatus().getReasonPhrase(),  ex.getMessage() )
         );
         return ResponseEntity.status(ex.getStatus()).body(body);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleArgumentException(Exception ex) {
+        Map<String, Object> body = Map.of(
+                "errorCode", HttpStatus.BAD_REQUEST.value(),
+                "errorMessage", String.format(errorForm, HttpStatus.BAD_REQUEST.getReasonPhrase(),  ex.getMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        Map<String, Object> body = Map.of(
+                "errorCode", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "errorMessage", String.format(errorForm, HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), ex.getMessage())
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
     }
 
 }
--- src/main/java/com/stock/batch/config/WebClientConfig.java
+++ src/main/java/com/stock/batch/config/WebClientConfig.java
@@ -1,11 +1,11 @@
 package com.stock.batch.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
